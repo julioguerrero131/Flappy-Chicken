074afd55bd7385f64cd31a7c12bb082483371771
Julio Guerrero Alvarez <116978348+julioguerrero131@users.noreply.github.com>
2025-05-29 23:47:53 -0500

    tutorial finished

--- Assets/Scripts/PipeMoveScript.cs
+++ Assets/Scripts/PipeMoveScript.cs
@@ -4,7 +4,6 @@ public class PipeMoveScript : MonoBehaviour
 {
     public float moveSpeed = 10;
     public float deadZone = -40;
-    public bool isPaused = false;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -15,19 +14,11 @@ public class PipeMoveScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (!isPaused)
+        transform.position = transform.position + (Vector3.left * moveSpeed) * Time.deltaTime;
+
+        if (transform.position.x < deadZone)
         {
-            transform.position = transform.position + (Vector3.left * moveSpeed) * Time.deltaTime;
-
-            if (transform.position.x < deadZone)
-            {
-                Destroy(gameObject);
-            }
+             Destroy(gameObject);
         }
-    }
-
-    void setPaused(bool paused)
-    {
-        isPaused = paused;
     }
 }
